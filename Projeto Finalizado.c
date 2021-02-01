0e0ecde8948ad15a6f19c3de3f594c0a869dafe0
JackssonYuri <78384230+JackssonYuri@users.noreply.github.com>
2021-02-01 18:03:44 -0300

    Add files via upload

---
+++ Projeto Finalizado.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#define MIN_MATRIZ 10
+#define MAX_MATRIZ 81
+#define MAX_PALAVRA 10
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese"); // para acentuação.
+    char matriz[MIN_MATRIZ][MAX_MATRIZ];
+    char palavra[MAX_PALAVRA], inverso[MAX_PALAVRA];
+
+    int encontrada, n, m, i, j, a, t; //n = dimensão da matriz; m = palavras a serem encontradas; p = palavras digitadas.
+
+    moduloLeitura(palavra, matriz, encontrada, n, m, i, j, a, t);
+
+}
+
+void moduloLeitura(char matriz[MIN_MATRIZ][MAX_MATRIZ], char palavra[1][MAX_PALAVRA]){
+    int encontrada; // NÃO = 0; SIM = 1.
+    int n, m, i, j, a, t;
+      //n = dimensão; m = qntd de palavras; i = contador 1; j = contador 2; pal = palavras na matriz; c = coluna; l = linha.
+
+        printf("\t\tJOGO DE CAÇA-PALAVRAS\n\n\tBEM VINDO AO JOGO DE CAÇA-PALAVRAS!\n\n");
+        printf("PARA COMEÇAR O JOGO VOCÊ DEVE DIGITAR O TAMANHO DA MATRIZ.\n*****Obs: A matriz deve ter dimensão entre 10 e 80.*****\n");
+        printf("DIGITE A DIMENSÃO DA MATRIZ: \n");
+        scanf("%d", &n);
+
+        if(n>=10 && n <= 80){
+            printf("AGORA VOCÊ DEVE DIGITAR AS LETRAS DA MATRIZ!\n");
+            printf("DIGITE AS LETRAS DA MATRIZ:\n");
+
+            for (i = 0; i < n; i++)
+                scanf("%s", matriz[i]); //Armazena as letras na matriz.
+
+            printf("AGORA VOCÊ DEVE DIGITAR O NÚMERO DE PALAVRAS QUE DESEJA:\n*****Obs: Até 10.*****\n");
+            scanf("%d", &m); //Armaze a quantidade de palavras que o usuário deseja encontrar na matriz.
+
+
+            printf("DIGITE AS PALAVRAS ENCONTRADAS:\n");
+
+
+
+
+            for (i = 0; i < m; i++)
+                scanf("%s", palavra[i]); //Armazena as palavras que serão buscadas na matriz.
+
+                printf("RESULTADO:\n");
+
+                for (a = 0; a < m ; a++)
+                {
+                    encontrada = 0; // não encontrada.
+
+                    for(i = 0; i < n; i++) //para linha
+                    {
+                        for(j = 0; j < n; j++) //para coluna
+                        {
+                        if(palavra[a][0] == matriz[i][j]) // laço para encontrar as palavras digitadas na matriz.
+                            {
+                                if(palavra[a][1] == matriz[i - 1][j])
+                                {
+                                    encontrada = 1;
+
+                                    for(t = 0; t < strlen(palavra[a]); t++)
+                                        if( palavra[a][t] != matriz[i - t][j])
+                                            encontrada = 0;
+
+                                        if (encontrada)
+                                            break;
+
+
+                                }
+                                if (palavra[a][1] == matriz[i + 1][j]) //Se a segunda letra for encontrada abaixo (Sul)
+                                {
+                                    encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i + t][j])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i][j + 1]) //Se a segunda letra for encontrada a direita (Leste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i][j + t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i][j - 1]) //Se a segunda letra for encontrada a esquerda (Oeste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i][j - t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i - 1][j + 1]) //Se a segunda letra for encontrada acima-direita (Nordeste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i - t][j + t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i - 1][j - 1]) //Se a segunda letra for encontrada acima-esquerda (Noroeste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i - t][j - t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i + 1][j + 1]) //Se a segunda letra for encontrada abaixo-direita (Sudeste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i + t][j + t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+
+                    if (palavra[a][1] == matriz[i + 1][j - 1]) //Se a segunda letra for encontrada abaixo-esquerda (Sudoeste)
+                    {
+                        encontrada = 1;
+
+                        for (t = 0; t < strlen(palavra[a]); t++)
+                            if (palavra[a][t] != matriz[i + t][j - t])
+                                encontrada = 0;
+
+                        if (encontrada)
+                            break;
+                    }
+                }
+            }
+
+            if (encontrada)
+                break;
+        }
+
+                                if(encontrada){
+                                    printf("%s FOI ENCONTRADA\n", palavra[a]);
+
+                                    for ( i = 0; i < n; i++){
+                                            for( j = 0; j < n; j++){
+                                            printf("%d ", matriz[i][j]);
+                                            }
+                                    }
+
+                                    }else{
+                                    printf("%s NÃO FOI ENCONTRADA\n", palavra[a]);
+                                    }
+                }
+
+
+                }//1ªIF
+
+                else { //else do primeiro if, caso a matriz esteja menor que 10 e maior que 80.
+                    printf("DIMENSÃO INCORRETA!\n");
+
+                }
+                //else
+
+                }//DO MAIN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
